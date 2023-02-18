6f9482dd632ceb83cbb4085760f73eff328ae60c
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2023-02-18 12:06:27 -0300

    create board

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react"
-import App from "./App"
+import { App } from "./App"
 
 describe('<App>', ()=> {
   it('renders without crashing', () => {
-    render(
+    const { container } = render(
       <App />
     )
 
-    expect(screen.getByText('Hello World!')).toBeInTheDocument()
+    expect(container.querySelector('#board')).toBeInTheDocument()
   })
 })
